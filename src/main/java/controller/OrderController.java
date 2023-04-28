a0a0d5c5cd46e0d769ea972f0a0acbfe0cd976ed
new_user <nrustgen@gmail.com>
2023-04-27 17:16:34 -0700

    writing log with a liquibase problem 2

--- src/main/java/controller/OrderController.java
+++ src/main/java/controller/OrderController.java
@@ -2,7 +2,8 @@ package controller;
 
 import dto.OrderDetailsDTO;
 import model.Order;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -16,8 +17,13 @@ import java.util.Optional;
 @RequestMapping("/api/orders")
 public class OrderController {
 
-    @Autowired
-    private OrderService orderService;
+    private static final Logger LOGGER = LoggerFactory.getLogger(OrderController.class.getSimpleName());
+
+    private final OrderService orderService;
+
+    public OrderController(OrderService orderService) {
+        this.orderService = orderService;
+    }
 
     @PostMapping
     public Order save(@RequestBody Order order) {
